4a5f03d68b124cf8495a4587a90afd2f65bd05c2
Ryan <ryan.tjong888@gmail.com>
2020-09-26 23:24:25 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+// import firebase from './firebase';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      holidays: [],
+      date: []
+    }
+  }
+
+  // component is mounted after the first rendering
+  componentDidMount(){
+    // make an AJAX request for art
+    axios({
+      method: 'GET',
+      url: ' 	https://calendarific.com/api/v2/holidays?api_key=8c48c3bc158c0a26c6e03d266e8ec44368d0898f&year=2020&country=ca&type=national',
+      dataResponse: 'json',
+    }).then(results => {
+      const nationalHolidays = results.data.response.holidays
+      console.log(nationalHolidays);
+
+      const fuck = [];
+      console.log(fuck);
+      nationalHolidays.forEach(holiday => {
+        fuck.push(holiday.name)
+        // console.log(holiday.name);
+        this.setState({
+          holidays: fuck,
+          // date: holidayDate
+        })
+      });
+
+      
+
+
+      // for (let index = 0; index < nationalHolidays.length; index++) {
+      //   const holidayName = nationalHolidays;
+      //   const holidayDate = nationalHolidays[index].date.iso;
+        // console.log(holidayName);
+        // console.log(holidayDate);
+
+        // const newState = [];
+        // for (const key in nationalHolidays) {
+        //   newState.push({
+        //     key: key,
+        //     title: nationalHolidays[key]
+        //   });
+          
+        //   console.log(newState); 
+
+          
+        // }
+      // }
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Holiday Planner</h1>
+        {this.state.holidays.map(test => {
+          return (
+            <p>{test}</p>
+          )
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';    //firebase
+import 'firebase/database';             //firebase database
+
+//copy and pasted from realtime database
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDLPmBSOJArNcPL9QnPglUBmu5bKjNyt9g",
+    authDomain: "projectfive-bc68e.firebaseapp.com",
+    databaseURL: "https://projectfive-bc68e.firebaseio.com",
+    projectId: "projectfive-bc68e",
+    storageBucket: "projectfive-bc68e.appspot.com",
+    messagingSenderId: "259913504488",
+    appId: "1:259913504488:web:8589d72bf59294ef1cdb5d"
+    };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
