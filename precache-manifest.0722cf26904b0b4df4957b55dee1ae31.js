2a9842dcf0f415a49b9b41b6123fff28cd7c8efd
RyanTjong88 <ryan.tjong888@gmail.com>
2020-10-15 00:03:45 -0400

    Updates

---
+++ precache-manifest.0722cf26904b0b4df4957b55dee1ae31.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "911e7bc9ff2c69ecede446d995757a82",
+    "url": "/holidayPlanner/index.html"
+  },
+  {
+    "revision": "f47a14bde66f41b1eb69",
+    "url": "/holidayPlanner/static/css/main.7db2d4bc.chunk.css"
+  },
+  {
+    "revision": "baa7ce079532f63dc2db",
+    "url": "/holidayPlanner/static/js/2.9c672c0d.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/holidayPlanner/static/js/2.9c672c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f47a14bde66f41b1eb69",
+    "url": "/holidayPlanner/static/js/main.fbe6ec93.chunk.js"
+  },
+  {
+    "revision": "e3493722f6a381c6cada",
+    "url": "/holidayPlanner/static/js/runtime-main.39d74541.js"
+  },
+  {
+    "revision": "f8f0a6f9ca6e1b4cd13608d8abd853aa",
+    "url": "/holidayPlanner/static/media/boxingDayHeader.f8f0a6f9.jpg"
+  },
+  {
+    "revision": "5e075111f1c5d1c3a094765e6c7439ce",
+    "url": "/holidayPlanner/static/media/canadaDayHeader.5e075111.jpg"
+  },
+  {
+    "revision": "066a047604a1707f5773a63bdc44e0f8",
+    "url": "/holidayPlanner/static/media/christmasHeader.066a0476.jpg"
+  },
+  {
+    "revision": "800731fcf55a62c66b9879d8e0ecc945",
+    "url": "/holidayPlanner/static/media/easterHeader.800731fc.jpg"
+  },
+  {
+    "revision": "6f7543ff330707429c22d48cebd5cf97",
+    "url": "/holidayPlanner/static/media/goodFridayHeader.6f7543ff.jpg"
+  },
+  {
+    "revision": "f4c2a87f0409fbf4ce31ebd849894a76",
+    "url": "/holidayPlanner/static/media/labourDayHeader.f4c2a87f.jpg"
+  },
+  {
+    "revision": "6718812ad6e289418a0e5b9a7d5222dc",
+    "url": "/holidayPlanner/static/media/maldives.6718812a.jpg"
+  },
+  {
+    "revision": "6f6ef81e91604936b7d7e52cc9425b90",
+    "url": "/holidayPlanner/static/media/newYearsEveHeader.6f6ef81e.jpg"
+  },
+  {
+    "revision": "cce2ce8959ab5c2b06ba363f8c721107",
+    "url": "/holidayPlanner/static/media/remembranceDayHeader.cce2ce89.jpg"
+  },
+  {
+    "revision": "93455ee8501d2826e3e19b0338f8cc3e",
+    "url": "/holidayPlanner/static/media/thanksgivingHeader.93455ee8.jpg"
+  },
+  {
+    "revision": "bfdc0a00fc035c061d5d029b127263fa",
+    "url": "/holidayPlanner/static/media/victoriaDayHeadeer.bfdc0a00.jpg"
+  }
+]);
